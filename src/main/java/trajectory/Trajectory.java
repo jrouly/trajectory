e1f91394bcc003beef7dc7b8554d334e7ebacb1d
Jean Michel Rouly <michel.rouly@gmail.com>
2014-11-10 23:43:25 -0500

    Started laying out skeleton code for TM.

---
+++ src/main/java/trajectory/Trajectory.java
@@ -0,0 +1,21 @@
+package trajectory;
+
+/**
+ * The main executable class of the trajectory project to perform topic
+ * modeling.
+ */
+public class Trajectory {
+
+
+  /**
+   * Perform topic modeling on the data set as constrained by any command
+   * line arguments.
+   *
+   * @param args command line specifications
+   */
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+
+  }
+}
