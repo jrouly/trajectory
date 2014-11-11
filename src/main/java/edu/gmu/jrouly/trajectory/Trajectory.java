18c34771dd412821a3625790ad37a8b8c8167750
Jean Michel Rouly <michel.rouly@gmail.com>
2014-11-11 04:41:38 -0500

    Added tests.

--- src/main/java/edu/gmu/jrouly/trajectory/Trajectory.java
+++ src/main/java/edu/gmu/jrouly/trajectory/Trajectory.java
@@ -9,8 +9,6 @@ import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-// import java.io.InputStream;
-// import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.Reader;
 import java.io.InputStreamReader;
@@ -24,6 +22,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.TreeSet;
 import java.util.regex.Pattern;
 
 import cc.mallet.pipe.CharSequence2TokenSequence;
@@ -36,8 +38,14 @@ import cc.mallet.pipe.TokenSequenceRemoveStopwords;
 import cc.mallet.pipe.iterator.FileIterator;
 
 import cc.mallet.topics.ParallelTopicModel;
+import cc.mallet.topics.TopicInferencer;
 
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.FeatureSequence;
+import cc.mallet.types.IDSorter;
+import cc.mallet.types.LabelSequence;
 import cc.mallet.types.InstanceList;
+import cc.mallet.types.Instance;
 
 /**
  * The main executable class of the trajectory project to perform topic
@@ -128,7 +136,7 @@ public class Trajectory {
     // statistics after every iteration.
     model.setNumThreads(2);
 
-    // Run the model for 50 iterations and stop (this is for testing only, 
+    // Run the model for 50 iterations and stop (this is for testing only,
     //  for real applications, use 1000 to 2000 iterations)
     model.setNumIterations(50);
     try {
@@ -138,6 +146,72 @@ public class Trajectory {
       System.err.println( "Error estimating topics when reading from data set." );
       System.exit( 1 );
     }
+
+
+
+    // Show the words and topics in the first instance
+
+    // The data alphabet maps word IDs to strings
+    Alphabet dataAlphabet = instances.getDataAlphabet();
+
+    FeatureSequence tokens = (FeatureSequence) model.getData().get(0).instance.getData();
+    LabelSequence topics = model.getData().get(0).topicSequence;
+
+    // For each token in the 0th instance, print out the topic it is
+    // associated with.
+    Formatter out = new Formatter(new StringBuilder(), Locale.US);
+    for (int position = 0; position < tokens.getLength(); position++) {
+        out.format("%s-%d ", dataAlphabet.lookupObject(tokens.getIndexAtPosition(position)), topics.getIndexAtPosition(position));
+    }
+    System.out.println(out);
+
+    // Estimate the topic distribution of the first instance,
+    // given the current Gibbs state.
+    double[] topicDistribution = model.getTopicProbabilities(0);
+
+    // Get an array of sorted sets of word ID/count pairs
+    ArrayList<TreeSet<IDSorter>> topicSortedWords = model.getSortedWords();
+
+    // Show top 5 words in topics with proportions for the first document
+    for (int topic = 0; topic < numTopics; topic++) {
+        Iterator<IDSorter> iterator = topicSortedWords.get(topic).iterator();
+
+        out = new Formatter(new StringBuilder(), Locale.US);
+        out.format("%d\t%.3f\t", topic, topicDistribution[topic]);
+        int rank = 0;
+        while (iterator.hasNext() && rank < 5) {
+            IDSorter idCountPair = iterator.next();
+            out.format("%s (%.0f) ", dataAlphabet.lookupObject(idCountPair.getID()), idCountPair.getWeight());
+            rank++;
+        }
+        System.out.println(out);
+    }
+
+    // Create a new instance with high probability of topic 0
+    StringBuilder topicZeroText = new StringBuilder();
+    Iterator<IDSorter> iterator = topicSortedWords.get(0).iterator();
+
+    int rank = 0;
+    while (iterator.hasNext() && rank < 5) {
+        IDSorter idCountPair = iterator.next();
+        topicZeroText.append(dataAlphabet.lookupObject(idCountPair.getID()) + " ");
+        rank++;
+    }
+
+    // Create a new instance named "test instance" with empty target and source fields.
+    InstanceList testing = new InstanceList(instances.getPipe());
+    testing.addThruPipe(new Instance(topicZeroText.toString(), null, "test instance", null));
+
+    TopicInferencer inferencer = model.getInferencer();
+    double[] testProbabilities = inferencer.getSampledDistribution(testing.get(0), 10, 1, 5);
+    System.out.println("0\t" + testProbabilities[0]);
+
+
+
+
+
+
+
 
 
   }
