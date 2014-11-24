1d0bacfbddcb4aa2d2bf659e1d5e769c89745b10
Jean Michel Rouly <michel.rouly@gmail.com>
2014-11-24 03:12:59 -0500

    Added some edge cases and correted the cleaning func

--- src/main/python/trajectory/scrape/engines/pdx_chem.py
+++ src/main/python/trajectory/scrape/engines/pdx_chem.py
@@ -61,12 +61,24 @@ def scrape( args, data_path ):
     log.debug( "Looping over course list." )
     for course in course_list:
 
-        syllabus_link = url_fix( course.get("href") )
-        log.debug( "Course URL: " + course.get("href") )
-        syllabus = get( syllabus_link, stream=True )
+        syllabus_link = course.get("href")
+        if syllabus_link is None:
+            log.debug("Syllabus link not found.")
+            continue
+
+        syllabus_link = url_fix( syllabus_link )
+        log.debug( "Course URL: " + syllabus_link )
+
+        try:
+            syllabus = get( syllabus_link, stream=True )
+        except Exception:
+            # Something went wrong
+            log.debug("Invalid URL.")
+            continue
 
         if not syllabus.ok:
             # Something went wrong
+            log.debug("Error downloading syllabus.")
             continue
 
         # Write syllabus to disk.
@@ -85,47 +97,70 @@ def scrape( args, data_path ):
 
 def clean( args, raw_path, clean_path ):
     """
-    This function takes the pdx cs syllabi directory as input and removes
+    This function takes the pdx chem syllabi directory as input and removes
     all non-word elements from them.
     """
 
 
-    log.info( "Cleaning scraped PDX CS data." )
+    log.info( "Cleaning scraped PDX Chemistry data." )
 
 
+    # Generate a list of all pdf data files in the data path.
+    pdfs = [os.path.join(root, name)
+             for root, dirs, files in os.walk( raw_path )
+             for name in files
+             if name.endswith(".pdf")]
+
+    # Convert PDFs to text.
+    log.info( "Convert PDFs to text." )
+    from subprocess import call
+    try:
+        for pdf in pdfs:
+            filename = os.path.basename( pdf )[:-3] + "txt"
+            clean_file = os.path.join( clean_path, filename )
+            call(["pdftotext", pdf, clean_file])
+    except Exception as e:
+        log.error( "Unable to call system 'pdftotext' command." )
+        log.error( str(e) )
+
+
+    # Generate a list of the newly generated text files.
+    files = [os.path.join(root, name)
+             for root, dirs, files in os.walk( clean_path )
+             for name in files
+             if name.endswith(".txt")]
+
+
+    # Define cleaning regular expressions.
     whitespace = re.compile("\\\\n|\\\\r|\\\\xa0|\d|\W")
     singletons = re.compile("\s+\w{1,3}(?=\s+)")
     long_whitespace = re.compile("\s+")
 
-    # Generate a list of all data files in the data path.
-    files = [os.path.join(root, name)
-             for root, dirs, files in os.walk( raw_path )
-             for name in files
-             if name.endswith(".raw")]
 
-    # Iterate over each raw file
-    for raw_file in files:
+    # Iterate over each datafile
+    log.info( "Clean up text files." )
+    for datafile in files:
 
-        with open(raw_file, 'r') as socket:
-            contents = socket.read()
+        with open( datafile, "r" ) as content_file:
+            contents = content_file.read()
+
 
         # Perform regular expression substitutions.
         contents = re.sub(whitespace, ' ', contents) # remove non-letters
-        contents = re.sub(singletons, ' ', contents) # remove 1-2 letter words
+        contents = re.sub(singletons, ' ', contents) # remove single letters
         contents = re.sub(long_whitespace, ' ', contents)   # remove spaces
         contents = contents.lower()     # make everything lowercase
 
+
         # Trim syllabi with fewer than 500 characters, as they likely were
         # incorrectly cleaned.
-        if len( contents ) < 500:
+        if len( contents ) <= 500:
             log.debug("File contents too short, skipping.")
+            os.remove( datafile )
             continue
 
-        filename = os.path.basename( raw_file )[:-3] + "txt"
-        clean_file = os.path.join( clean_path, filename )
-
-        # Write out to a new file.
-        with open( clean_file, 'w' ) as out:
+        # Write out to the same file.
+        with open( datafile, 'w' ) as out:
             out.write( contents )
 
     log.info( "Completed data processing." )
