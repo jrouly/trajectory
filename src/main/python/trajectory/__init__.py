cb3fe71ae53b417f33635286084d2c35e01a8aa7
Jean Michel Rouly <michel.rouly@gmail.com>
2014-12-30 17:32:07 -0500

    Removed filesystem checks.

--- src/main/python/trajectory/__init__.py
+++ src/main/python/trajectory/__init__.py
@@ -29,25 +29,6 @@ def scrape(args):
     log.info("Selected scraping targets: %s." % args.targets)
 
 
-    # Create data directory, if it doesn't already exist.
-    # Also make sure that the raw/clean subdirectories exist within it.
-    if not os.path.exists( args.data_dir ):
-        log.info("Data directory \"%s\" does not exist. Creating." % args.data_dir)
-        os.makedirs( args.data_dir )
-
-    # Ensure raw path exists.
-    raw_path = os.path.join( args.data_dir, "raw" )
-    if not os.path.exists( raw_path ):
-        log.info("Raw data directory \"%s\" does not exist. Creating." % raw_path )
-        os.makedirs( raw_path )
-
-    # Ensure clean path exists.
-    clean_path = os.path.join( args.data_dir, "clean" )
-    if not os.path.exists( clean_path ):
-        log.info("Clean data directory \"%s\" does not exist. Creating." % clean_path )
-        os.makedirs( clean_path )
-
-
     # Loop over the requested targets and call their scrape function.
     for target in args.targets:
 
@@ -60,89 +41,24 @@ def scrape(args):
         scraper = import_module( target_module, "trajectory.engines" )
 
 
-        # Define the raw and clean paths specific to this target engine.
-        target_raw_path = os.path.join( raw_path, target )
-        target_clean_path = os.path.join( clean_path, target )
-
-
-        # If the purge argument is flagged, run it.
-        if args.purge:
-            import shutil
-            try:
-
-                # Remove the entire tree struct for raw and clean data.
-                if os.path.exists( target_raw_path ):
-                    shutil.rmtree( target_raw_path )
-                if os.path.exists( target_clean_path ):
-                    shutil.rmtree( target_clean_path )
-
-            except Exception as e:
-                log.warn("Error occurred when purging target data.")
-                log.debug( e )
-
-        else:
-            log.info("No data to remove.")
-
-
-        # If downloading is flagged, run it.
         if args.download:
             try:
-
-
-                # If the target raw path does not exist, create it.
-                # If it does exist, then warn the user.
-                if not os.path.exists( target_raw_path ):
-                    log.info("\"%s\" does not exist. Creating..." %
-                            target_raw_path )
-                    os.makedirs( target_raw_path )
-                else:
-                    log.info("\"%s\" already exists." % target_raw_path )
-
-
-                # Run the engine's scraper method.
                 scraper.scrape( args, target_raw_path )
-
-
             except NotImplementedError as e:
-
-
-                # If the scraper function isn't defined, throw up
-                # gracefully and continue.
                 log.warn( "Target %s has not been defined. Skipping." %
                         target )
 
 
-        # If cleaning is flagged, run it.
         if args.digest:
             try:
-
-
-                # If the target clean path does not exist, create it.
-                # If it does exist, then warn the user.
-                if not os.path.exists( target_clean_path ):
-                    log.info("\"%s\" does not exist. Creating..." %
-                            target_clean_path )
-                    os.makedirs( target_clean_path )
-                else:
-                    log.info("\"%s\" already exists." % target_clean_path )
-
-
-                # Run the engine's clean method.
                 scraper.clean( args, target_raw_path, target_clean_path )
-
-
             except NotImplementedError as e:
-
-
-                # If the clean function isn't defined, throw up gracefully
-                # and continue.
                 log.warn( "Target %s has not been defined. Skipping." %
                         target )
 
 
-        # If neither clean nor download were flagged, notify the user.
-        if not (args.digest or args.download or args.purge):
-            log.warn("Heads up, no action performed.")
+        if not (args.digest or args.download):
+            log.warn("No action performed.")
 
 
         log.info("Disengaging scraper engine.")
