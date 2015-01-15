9a28952200d0cd0fa7f7fc56c0d6a3c826c4c034
Jean Michel Rouly <michel.rouly@gmail.com>
2015-01-14 19:56:49 -0500

    Removed sql files for registration

--- src/main/resources/sql/register-dept.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO 'Departments' ('SchoolID', 'Name', 'Abbreviation', 'Web')
-VALUES('%(schoolid)s', '%(name)s', '%(abbrev)s', '%(web)s');
--- src/main/resources/sql/register-school.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO 'Schools' ('Name', 'Abbreviation', 'Web')
-VALUES('%(name)s', '%(abbrev)s', '%(web)s');
