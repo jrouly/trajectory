a628a409140eaf3e75a2dceb68ab1c4be8efe2e3
Jean Michel Rouly <michel.rouly@gmail.com>
2015-02-24 13:20:11 -0500

    cleaned up docs

--- src/main/python/trajectory/models/__init__.py
+++ src/main/python/trajectory/models/__init__.py
@@ -1,3 +1,11 @@
+"""
+trajectory/models/__init__.py
+Author: Jean Michel Rouly
+
+Collect the models and database meta objects into one module.
+"""
+
+
 from trajectory.models import meta
 from trajectory.models.models import University, Department, Course
 
--- src/main/python/trajectory/models/meta.py
+++ src/main/python/trajectory/models/meta.py
@@ -1,12 +1,17 @@
+"""
+trajectory/models/meta.py
+Author: Jean Michel Rouly
+
+Define references to the database meta objects to allow reference on a
+global scale.
+"""
+
+
 from trajectory import constants as TRJ
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-"""
-Define references to the database meta objects to allow reference on a
-global scale.
-"""
 
 Engine = create_engine(TRJ.DATABASE_URI)
 Base = declarative_base()
