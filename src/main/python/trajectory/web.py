a020fdc8dd54ac755e15433255a8165cdffc126a
Jean Michel Rouly <michel.rouly@gmail.com>
2015-03-09 21:39:23 -0400

    Pass data from models into templates correctly.

--- src/main/python/trajectory/web.py
+++ src/main/python/trajectory/web.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, abort
+from flask import make_response, request
 from jinja2 import FileSystemLoader
+from sqlalchemy.sql.expression import func
+
 from trajectory import config as TRJ
+from trajectory.models import University, Department, Course, ResultSet
+from trajectory.models import Topic, CourseTopicAssociation
+from trajectory.models.meta import Session
+
+
+#####################
+# Application Setup #
+#####################
 
 app = Flask(__name__)
 app.config.from_object(dict(
     DEBUG = True,
-    SQLALCHEMY_DATABASE_URI = TRJ.DATABASE_URI,
     THREADS_PER_PAGE = 8,
 ))
 app.jinja_loader = FileSystemLoader(TRJ.TEMPLATES)
-app.db = SQLAlchemy(app)
+app.db = Session()
 
+
+###################
+# View Definition #
+###################
 
 # Manage error handling.
 @app.errorhandler(404)
@@ -21,29 +34,70 @@ def not_found(error):
 # Define routing for dashboard page.
 @app.route('/')
 def dashboard():
-    return "Dashboard."
+    universities = app.db.query(University).all()
+    result_sets = app.db.query(ResultSet).all()
+    response = make_response(render_template(
+            "index.html",
+            universities=universities,
+            result_sets=result_sets))
+
+    # Set the 'active' result set as the first available one.
+    if len(result_sets) > 0:
+        response.set_cookie('result_set', str(result_sets[0].id))
+
+    return response
 
 # Define routing for university list.
 @app.route('/universities/')
 def university_list():
-    return "University list."
+    universities = app.db.query(University).all()
+    return render_template("universities.html", universities=universities)
 
 # Define routing for university index pages.
-@app.route('/universities/<string:university_abbr>/')
-def university(university_abbr=None):
-    return "University: %s" % university_abbr
+@app.route('/universities/<string:u>/')
+def university(u=None):
+    university = app.db.query(University) \
+            .filter(University.abbreviation==u).first()
+    if university is None:
+        abort(404) # university not found
+    return render_template("university.html", university=university)
 
 # Define routing for departmental pages.
-@app.route('/universities/<string:university_abbr>/<string:dept_abbr>/')
-def department(university_abbr=None, dept_abbr=None):
-    return "Uni %s Dept %s" % (university_abbr, dept_abbr)
+@app.route('/universities/<string:u>/<string:d>/')
+def department(u=None, d=None):
+    department = app.db.query(Department).join(University) \
+            .filter(University.abbreviation==u) \
+            .filter(Department.abbreviation==d).first()
+    if department is None:
+        abort(404) # department not found
+    return render_template("department.html", department=department)
 
 # Define routing for topic list.
 @app.route('/topics/')
 def topics():
-    return "Topics."
+    topics = app.db.query(Topic) \
+            .join(CourseTopicAssociation) \
+            .group_by(Topic.id) \
+            .order_by(func.count().desc()) \
+            .all()
+    return render_template("topics.html", topics=topics)
 
 # Define routing for about page.
 @app.route('/about/')
 def about():
     return render_template("about.html")
+
+
+######################
+# Filter Definitions #
+######################
+
+@app.template_filter('course_count')
+def course_count(data):
+    if type(data) == University:
+        return sum([len(department.courses)
+            for department in data.departments])
+    elif type(data) == Department:
+        return len(data.courses)
+    else:
+        return 0
