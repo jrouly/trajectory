fca5cfaff2fe708f233f44ad70a653295d7b4a42
Jean Michel Rouly <michel.rouly@gmail.com>
2015-03-18 23:38:28 -0400

    fleshed out function to vectorize by topics

--- src/main/python/trajectory/utils/common.py
+++ src/main/python/trajectory/utils/common.py
@@ -28,10 +28,58 @@ def jaccard(a, b):
     return n / d
 
 
-def topic_vector(item):
+def topic_vector(item, result_set=None):
     """
     Generate a vector indicating which topics are represented by the given
     item.
+
+    item can be a University, Department, or Course.
     """
 
-    pass
+    from trajectory.models import University, Department, Course, Topic
+    from trajectory.models import CourseTopicAssociation
+    from trajectory.models.meta import session
+    from bitarray import bitarray
+
+    # Generate the initial query. Note that if a result_set instance is
+    # passed in, it will be used to filter the global topics.
+    topics_query = session.query(Topic).order_by(Topic.id)
+    if result_set is not None:
+        topics_query = topics_query.filter(Topic.result_set_id==result_set.id)
+    global_topics = topics_query.all()
+
+    # Start constructing the per-item topic list.
+    item_topics = topics_query.join(CourseTopicAssociation).join(Course)
+
+    # Add any additional filtration for course/department/university.
+    if type(item) == Course:
+        item_topics = item_topics \
+                .filter(Course.id==item.id) \
+                .all()
+
+    elif type(item) == Department:
+        item_topics = item_topics \
+                .join(Department) \
+                .filter(Department.id==item.id) \
+                .all()
+
+    elif type(item) == University:
+        item_topics = item_topics \
+                .join(Department) \
+                .join(University) \
+                .filter(University.id==item.id) \
+                .all()
+
+    else:
+        raise RuntimeError("Unknown item type for topic vector.")
+
+    # Construct the vector.
+    vector = bitarray(len(global_topics))
+    vector.setall(False)
+
+    # Set the bits where the topic is found.
+    for it in item_topics:
+        if it in global_topics:
+            vector[global_topics.index(it)] = True
+
+    return vector
